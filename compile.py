dcf1269b53661dad2cc201c90aeb0f0e781cfca0
ICTman1076 <ict@stch.tk>
2019-04-12 21:36:07 +0100

    Finish compile.py, new compiled file
    I pretty printed the compiled file for ease of human

--- compile.py
+++ compile.py
@@ -1,29 +1,73 @@
+print("Compiling Advanced Mode Raw Data.")
+print("(c) Advanced Mode Foundation 2019")
+print("=================================\n")
 import json
 
-# load in the raw data
+print("Loading in raw data...")
 rulesData = []
 with open("./json-data/rules.json") as f:
     rulesData = json.loads(f.read())
 
-# prepare the compiled data
-rulesCompiled = {
-    "rules": {},
-    "aliases": {}
+styleData = []
+with open("./json-data/styles.json") as f:
+    styleData = json.loads(f.read())
+
+variationsData = []
+with open("./json-data/variations.json") as f:
+    variationsData = json.loads(f.read())
+
+print("Compiling...")
+# prepare the compiled data structure
+compiledData = {
+    "rules": {
+        "data": {},
+        "aliases": {}
+    },
+    "styles": {
+        "data": {},
+        "aliases": {},
+    },
+    "variations": {
+        "data": {},
+        "aliases": {}
+    }
 }
 
-for i in rulesData:
-    # find the identifier & aliases
-    identifier = i["identifier"]
-    aliases = i["aliases"]
+def compile(data):
+    miniCompiled = {
+        "data": {},
+        "aliases": {}
+    }
+    for i in data:
+        # find the identifier & aliases
+        identifier = i["identifier"]
+        aliases = i["aliases"]
 
-    # compile the aliases
-    for j in aliases:
-        rulesCompiled["aliases"][j] = identifier
+        # compile the aliases
+        for j in aliases:
+            miniCompiled["aliases"][j] = identifier
 
-    # compile the rules
-    rulesCompiled["rules"][identifier] = i
-    del rulesCompiled["rules"][identifier]["aliases"]
+        # compile the rules
+        miniCompiled["data"][identifier] = i
+        #del miniCompiled["data"][identifier]["aliases"]
+    return miniCompiled
 
+print(" - Rules... ", end="")
+compiledData["rules"] = compile(rulesData)
+print("Done!")
+
+print(" - Styles... ",end="")
+compiledData["styles"] = compile(styleData)
+print("Done!")
+
+print(" - Variations...",end="")
+compiledData["variations"] = compile(variationsData)
+print("Done!")
+
+print("Saving... ",end="")
 # save the compiled data
-with open("./compiled/rules.json", 'w') as f:
-    f.write(json.dumps(rulesCompiled))
+with open("./compiled/advancedModeData.json", 'w') as f:
+    f.write(json.dumps(compiledData))
+print("Saved.")
+
+input("Press enter to exit.")
